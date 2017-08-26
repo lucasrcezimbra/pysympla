a70dfd3ce6355383acbe6746edd6ff82c30f7430
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2017-08-26 15:42:23 -0300

    Add property title to event

--- pysympla.py
+++ pysympla.py
@@ -35,20 +35,28 @@ class Sympla:
 
 class Event:
     def __init__(self, html):
-        self.soup = BeautifulSoup(html, 'html5lib')
+        self._soup = BeautifulSoup(html, 'html5lib')
         self._confirmed_participants = 0
         self._pending_participants = 0
+        self._title = ''
 
     @property
     def confirmed_participants(self):
         if not self._confirmed_participants:
             id = 'spanTotalParticipants'
-            self._confirmed_participants = self.soup.find(id=id).get_text()
+            self._confirmed_participants = self._soup.find(id=id).get_text()
         return self._confirmed_participants
 
     @property
     def pending_participants(self):
         if not self._pending_participants:
             id = 'spanTotalPendingParticipants'
-            self._pending_participants = self.soup.find(id=id).get_text()
+            self._pending_participants = self._soup.find(id=id).get_text()
         return self._pending_participants
+
+    @property
+    def title(self):
+        if not self._title:
+            id = 'event-title'
+            self._title = self._soup.find(id=id).get_text()
+        return self._title
