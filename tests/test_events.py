9ac9599ae922616237a3022e70f9437fc63f9b63
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2017-10-24 21:35:48 -0200

    Improve tests and add more

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pysympla.sympla import Event 
+from pysympla.sympla import Event
 
 
 @pytest.fixture
@@ -29,10 +29,12 @@ def event(html_event):
 
 
 def test_confirmed_participants(event):
+    assert type(event.confirmed_participants) == int
     assert event.confirmed_participants == 2
 
 
 def test_pending_participants(event):
+    assert type(event.pending_participants) == int
     assert event.pending_participants == 0
 
 
