3789d5049763fba73091b4778c5162b7763b3fce
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2017-10-24 12:35:26 -0200

    Add PyPi classifiers

--- setup.py
+++ setup.py
@@ -21,4 +21,20 @@ if __name__ == "__main__":
         packages=['pysympla'],
         zip_safe=False,
         include_package_data=True,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Natural Language :: Portuguese (Brazilian)',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3 :: Only',
+        ])
     )
